ab932e8800b5bf470e365a53936d19dc578c2e01
tagiordano88 <tiffany.giordano.j0ho@statefarm.com>
2021-07-09 14:28:45 -0500

    completed all steps

--- CellPhone.js
+++ CellPhone.js
@@ -1,6 +1,61 @@
 class CellPhone {
+    constructor (card){
+        this.card = card;
+        this.phoneNumber = "";
+        this.talking = false;
+        this.minutesUsed = 0;
+        this.callHistory = "";  
+        this.terminated = false;      
 
+    }
+
+    call(phoneNumber){
+        this.talking = true;
+        this.phoneNumber = phoneNumber;
+    }
+
+    isTalking() {
+        return this.talking;
+    }
+
+    tick() {    
+        this.minutesUsed ++;
+        if (this.card.minutes <= 1){
+          this.card.minutes --;
+          this.terminated = true;
+          this.endCall();
+        } else {
+        this.card.minutes --;    
+        }
+    }
+
+    endCall() {
+        this.talking = false;
+        let minutes = "";
+        if(this.minutesUsed <= 1){
+            minutes = "minute";
+        } else {
+            minutes = "minutes";
+        }
+        let terminateMessage = '';
+        if (this.terminated){
+           terminateMessage = 'cut off at '
+        }
+        
+        if(this.callHistory.length > 1){
+            this.callHistory = this.callHistory.concat(", " + this.phoneNumber + " (" + terminateMessage + this.minutesUsed + " " + minutes +")");
+        } 
+        else {
+            this.callHistory = this.callHistory.concat(this.phoneNumber + " (" + terminateMessage + this.minutesUsed + " " + minutes +")");
+        }
+        this.minutesUsed = 0;
+       
+    }
+
+    getHistory() {        
+       return console.log(this.callHistory);
+    }
 
 }
 
-module.export = CellPhone;
+module.exports = CellPhone;
--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 
 
 const CallingCard = require('./CallingCard.js');
-const CallingCard = require('./CellPhone.js');
+const CellPhone = require('./CellPhone.js');
 
-let card1 = new CallingCard(11);
-card1.addDollars(3);         // 300 cents / 11 = 27.2727272... = 27 minutes added
-card1.getRemainingMinutes(); // => returns 27
+let card = new CallingCard(20);
+card.addDollars(1); // add 100 cents @ 20 cents/minute = 5 minutes added
 
-let card2 = new CallingCard(17);
-card2.addDollars(3);         // 300 cents / 17 = 17.647... = 17 minutes added
-card2.getRemainingMinutes(); // => returns 17
-console.log(card1, card2);
+let phone = new CellPhone(card);
+phone.call("555-1111");
+phone.tick();       // 1 minute elapsed
+phone.tick();       // 2 minutes elapsed
+phone.endCall();
+
+phone.call("555-3333");
+phone.tick();       // 3 minutes elapsed
+phone.tick();       // 4 minutes elapsed
+phone.tick();       // this is the end of the 5th minute, so the call is ended
+
+phone.getHistory(); // => returns "555-1111 (2 minutes), 555-3333 (cut of at 3 minutes)"
+
+card.getRemainingMinutes() // => returns 0, because all 5 minutes have been used up
+console.log(phone);
